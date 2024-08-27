155ec1a6efb627fea48362af39cec4d11839ee2e
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-08-27 16:12:25 -0300

    instalação do coverage

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -1,18 +1,39 @@
-import { expect, it, describe, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { expect, it, describe, beforeEach, afterEach } from "vitest";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import App from "../src/app/page";
 
 describe("App Component Test", () => {
   beforeEach(() => {
     render(<App />);
   });
+
+  afterEach(() => {
+    cleanup();
+  });
   //it.only testa apenas o teste que está sendo executado
+
   it("Login - Usuário", () => {
-    const inputUsuario = screen.findByPlaceholderText("Usuário");
+    const inputUsuario = screen.getByPlaceholderText("Usuário"); // Corrigido para getByPlaceholderText
     expect(inputUsuario).toBeDefined();
   });
+
   it("Login - Senha", () => {
-    const inputSenha = screen.findByPlaceholderText("Senha");
+    const inputSenha = screen.getByPlaceholderText("Senha"); // Corrigido para getByPlaceholderText
     expect(inputSenha).toBeDefined();
+  });
+
+  it("Input change", () => {
+    // Seleciona o input de usuário
+    const inputUsuario = screen.getByPlaceholderText(
+      "Usuário"
+    ) as HTMLInputElement; // Corrigido para getByPlaceholderText
+
+    // Simula a mudança de valor no input de usuário
+    fireEvent.change(inputUsuario, {
+      target: { value: "usuario@teste.com", name: "usuario" },
+    });
+
+    // Verifica se o valor foi atualizado corretamente
+    expect(inputUsuario.value).toBe("usuario@teste.com"); // Corrigido para comparar o valor
   });
 });
